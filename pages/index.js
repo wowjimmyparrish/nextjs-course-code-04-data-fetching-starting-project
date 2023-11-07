56ef783e731b83a358574a9501b6d7fe41f1427d
James Parrish <jthomasparrishjr@gmail.com>
2023-11-06 18:02:55 -0600

    update

---
+++ pages/index.js
@@ -0,0 +1,43 @@
+import fs from 'fs/promises';
+import path from 'path';
+import Link from 'next/link'; 
+
+function HomePage(props) {
+  const { products } = props;
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}><Link href={`/${product.id}`}>{product.title}</Link></li>
+      ))}
+    </ul>
+  );
+}
+  export async function getStaticProps(context) {
+    //get filepath using node js path and cwd -current working directory
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    if(!data) {
+      return {
+        redirect: {
+          destination: '/no-data',
+        }
+      };
+    }
+   
+    if (data.products.length === 0) {
+      return {
+        notFound: true,
+      };
+    }
+    return {
+      props: {
+        products: data.products,
+      },
+      revalidate: 10
+    };
+  }
+
+
+export default HomePage;
