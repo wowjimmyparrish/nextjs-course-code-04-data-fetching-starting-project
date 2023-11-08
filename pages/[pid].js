8947fdde63140d7864ac92a35f6aef0e3536254b
James Parrish <jthomasparrishjr@gmail.com>
2023-11-07 18:13:24 -0600

    implemented dynamic paths with fallback

--- pages/[pid].js
+++ pages/[pid].js
@@ -5,6 +5,10 @@ import path from 'path';
 function ProductDetailPage (props) {
     const { loadedProduct } = props;
 
+    // if (!loadedProduct) {
+    //   return <p>Loading...</p>
+    // }
+
   return (
    <Fragment>
     <h1>{loadedProduct.title}</h1>
@@ -12,14 +16,20 @@ function ProductDetailPage (props) {
    </Fragment>
   )
 }
-export async function getStaticProps(context) {
-  const { params } = context;
-  const productId = params.pid;
 
+async function getData() {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
+  return data
+}
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+
+  const data = await getData();
+  
   const product = data.products.find((product) => product.id === productId);
 
   return {
@@ -29,12 +39,13 @@ export async function getStaticProps(context) {
   }
 }
   export async function getStaticPaths() {
+    const data = await getData();
+
+    const ids = data.products.map((product) => product.id);
+    const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
     return {
-      paths: [
-        { params: { pid: 'p1' } },
-        { params: { pid: 'p2' } },
-        { params: { pid: 'p3' } }
-      ],
+      paths: pathsWithParams,
       fallback: false
     
   }
